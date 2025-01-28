e36c4a3480a5b77f7b3e1acdd385298feb0e04ca
A-Szymanowski <amycszyma@gmail.com>
2025-01-28 21:37:44 +0000

    creating static method

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! From Amy!");
+        System.out.println("Welcome to CS 112!");
+        printNumber();
     }
+//static method returns a number times ten
+    public static int timesTen(int number) {
+        return number * 10;
+    }
+//static method void
+    public static void printNumber(){
+        int number = 5;
+        System.out.println("Number " + number +" times 10 equals to " + timesTen(number));
+    }
+
 }
